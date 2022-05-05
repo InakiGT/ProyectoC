5601016087d77ab2b583fb25a5c62eadcf93f31c
Iñaki García <yarimfuego@gmail.com>
2022-05-04 20:42:02 -0500

    Agregada la funcion de eliminar un elemento de la base de datos y agregadas la funciones de buscar un elemento en la base de datos

--- main.c
+++ main.c
@@ -24,6 +24,33 @@ struct {
     int N;
 }BD;
 
+void prueba() {
+    for (int i = 0; i < BD.N; i++) {
+        printf("%s\n", BD.productos[i].nombre);
+    }
+}
+
+// Funciones auxiliares
+void showData(int n, PRODUCTO producto) {
+    printf("-----PRODUCTO-%i:\n", n);
+    printf("%s\n", producto.nombre );
+    printf("%s\n", producto.categoria);
+    printf("%i\n", producto.codigo);
+    printf("%i\n", producto.existencia);
+    printf("%lf\n", producto.precio_compra);
+    printf("%lf\n", producto.precio_venta);
+    printf("---CADUCIDAD\n");
+    printf("%i\n", producto.caducidad.dia);
+    printf("%s\n", producto.caducidad.mes);
+    printf("%i\n", producto.caducidad.anio);
+}
+
+void showAllData(int n, PRODUCTO data[]) {
+    for(int i = 0; i < n; i++) {
+        showData(i+1, data[i]);
+    }
+}
+
 // Leer Datos de la DB
 void readBinFile() {
 	FILE *file;
@@ -45,7 +72,35 @@ void addProduct(PRODUCTO product) {
 }
 
 void deleteProduct(int id) {
+    PRODUCTO *newData;
+    newData = malloc(sizeof(PRODUCTO)*BD.N-1);
+    int aux = 0;
+    for(int i = 0; i < BD.N; i++) {
+        if(id != BD.productos[i].codigo) {
+            newData[aux] = BD.productos[i];
+            aux++;
+        }
+    }
+    BD.N--;
+    BD.productos = newData;
+}
 
+void searchProductById(int id) {
+    for(int i = 0; i < BD.N; i++) {
+        if(id == BD.productos[i].codigo) {
+            showData(i+1, BD.productos[i]);
+            break;
+        }
+    }
+}
+
+void searchProductByName(char name[150]) {
+    for(int i = 0; i < BD.N; i++) {
+        if(strcmp(name, BD.productos[i].nombre) == 0) {
+            showData(i+1, BD.productos[i]);
+            break;
+        }
+    }
 }
 
 // Crear una instancia de los structs
@@ -64,11 +119,12 @@ FECHA createADate(int day, char month[50], int year) {
 }
 
 int main(void) {
-
-    // FECHA fecha = createADate(20, "Agosto", 2020);
-    // PRODUCTO producto = createAProduct("Coca", "bebidas", 2139013, 10, 10.00, 12.00, fecha);
-    // printf("%i %s %i", producto.existencia, producto.nombre, producto.codigo);
-
+    readBinFile();
+    FECHA fecha = createADate(20, "Agosto", 2020);
+    PRODUCTO producto = createAProduct("Seven-up", "bebidas", 2139013, 10, 10.00, 12.00, fecha);
+    addProduct(producto);
+    deleteProduct(22911095);
+    prueba();
 
     return 0;
 }
